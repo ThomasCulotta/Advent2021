7fbf9ed3a5a267d9ca22b9afe06a84eab4d49b12
Thomas <thomasculotta94@gmail.com>
2021-12-09 21:54:37 -0800

    minor cleanup for 10

--- Day10.py
+++ Day10.py
@@ -1,20 +1,6 @@
 filename = __file__.strip("py") + "txt"
 with open(filename, "r") as file:
-    data = file.readlines()
-
-scoreMap1 = {
-    ")" : 3,
-    "]" : 57,
-    "}" : 1197,
-    ">" : 25137
-}
-
-scoreMap2 = {
-    ")" : 1,
-    "]" : 2,
-    "}" : 3,
-    ">" : 4
-}
+    data = [line.strip() for line in file.readlines()]
 
 closeToOpen = {
     ")" : "(",
@@ -23,45 +9,57 @@ closeToOpen = {
     ">" : "<"
 }
 
-openToClose = {
-    "(" : ")",
-    "[" : "]",
-    "{" : "}",
-    "<" : ">"
-}
-
 openers = { "(", "[", "{", "<" }
 
 def Part1():
+    scoreMap1 = {
+        ")" : 3,
+        "]" : 57,
+        "}" : 1197,
+        ">" : 25137
+    }
+
     syntaxScore = 0
     for line in data:
         stack = []
-        for bracket in line.strip():
+        for bracket in line:
             if bracket in openers:
                 stack.append(bracket)
+            elif closeToOpen[bracket] == stack[-1]:
+                stack.pop()
             else:
-                if closeToOpen[bracket] == stack[-1]:
-                    stack.pop()
-                else:
-                    syntaxScore += scoreMap1[bracket]
-                    break
+                syntaxScore += scoreMap1[bracket]
+                break
 
     return syntaxScore
 
 def Part2():
+    scoreMap2 = {
+        ")" : 1,
+        "]" : 2,
+        "}" : 3,
+        ">" : 4
+    }
+
+    openToClose = {
+        "(" : ")",
+        "[" : "]",
+        "{" : "}",
+        "<" : ">"
+    }
+
     syntaxScores = []
     for line in data:
         syntaxScore = 0
         stack = []
-        for bracket in line.strip():
+        for bracket in line:
             if bracket in openers:
                 stack.append(bracket)
+            elif closeToOpen[bracket] == stack[-1]:
+                stack.pop()
             else:
-                if closeToOpen[bracket] == stack[-1]:
-                    stack.pop()
-                else:
-                    stack = []
-                    break
+                stack = []
+                break
 
         if len(stack) > 0:
             for bracket in stack[::-1]:
@@ -71,7 +69,7 @@ def Part2():
             syntaxScores.append(syntaxScore)
 
     syntaxScores = sorted(syntaxScores)
-    return syntaxScores[round(len(syntaxScores) / 2) - 1]
+    return syntaxScores[int(len(syntaxScores) / 2)]
 
 print(Part1())
 print(Part2())
