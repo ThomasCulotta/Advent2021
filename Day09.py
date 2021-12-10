18aeaea4cddf05c79ec688a798a6d0c3a4b37b4e
Thomas <thomasculotta94@gmail.com>
2021-12-09 22:01:03 -0800

    minor cleanup for 09

--- Day09.py
+++ Day09.py
@@ -2,40 +2,35 @@ filename = __file__.strip("py") + "txt"
 with open(filename, "r") as file:
     data = file.readlines()
 
-heightMap = [[int(height) for height in line[:-1]] for line in data]
-heightMapEx = heightMap.copy()
-heightMapEx.append([9 for index in range(len(heightMap[0]))])
-heightMapEx.insert(0, [9 for index in range(len(heightMap[0]))])
-
-for index in range(len(heightMapEx)):
-    heightMapEx[index].insert(0, 9)
-    heightMapEx[index].append(9)
+heightMap = [[9] + [int(height) for height in line[:-1]] + [9] for line in data]
+heightMap.append([9 for index in range(len(heightMap[0]))])
+heightMap.insert(0, [9 for index in range(len(heightMap[0]))])
 
 def Part1():
-    riskLevels = [point + 1 for y in range(len(heightMapEx)) for x in range(len(heightMapEx[y]))
-                  if (point := heightMapEx[y][x]) < heightMapEx[y - 1][x] and
-                     point < heightMapEx[y + 1][x] and
-                     point < heightMapEx[y][x - 1] and
-                     point < heightMapEx[y][x + 1]]
+    riskLevels = [point + 1 for y in range(len(heightMap)) for x in range(len(heightMap[y]))
+                  if (point := heightMap[y][x]) < heightMap[y - 1][x] and
+                     point < heightMap[y + 1][x] and
+                     point < heightMap[y][x - 1] and
+                     point < heightMap[y][x + 1]]
 
     return sum(riskLevels)
 
 def BasinSearch(x,y):
-    heightMapEx[y][x] = 9
+    heightMap[y][x] = 9
     basinSize = 1
 
-    if heightMapEx[y - 1][x] < 9: basinSize += BasinSearch(x, y - 1)
-    if heightMapEx[y + 1][x] < 9: basinSize += BasinSearch(x, y + 1)
-    if heightMapEx[y][x - 1] < 9: basinSize += BasinSearch(x - 1, y)
-    if heightMapEx[y][x + 1] < 9: basinSize += BasinSearch(x + 1, y)
+    if heightMap[y - 1][x] < 9: basinSize += BasinSearch(x, y - 1)
+    if heightMap[y + 1][x] < 9: basinSize += BasinSearch(x, y + 1)
+    if heightMap[y][x - 1] < 9: basinSize += BasinSearch(x - 1, y)
+    if heightMap[y][x + 1] < 9: basinSize += BasinSearch(x + 1, y)
 
     return basinSize
 
 def Part2():
     basins = []
-    for y in range(len(heightMapEx)):
-        for x in range(len(heightMapEx[y])):
-            if heightMapEx[y][x] < 9:
+    for y in range(len(heightMap)):
+        for x in range(len(heightMap[y])):
+            if heightMap[y][x] < 9:
                 basins.append(BasinSearch(x, y))
 
     max1 = max(basins)
