a90f936f0b521b3e0487f80068a732a0c76381f7
Thomas <thomasculotta94@gmail.com>
2021-12-12 22:33:03 -0800

    cleanup for 13

--- Day13.py
+++ Day13.py
@@ -6,67 +6,41 @@ dots = [(int(pair[0]), int(pair[1])) for line in data if len(pair := line.split(
 intstructions = [((pair := line.split("="))[0][-1], int(pair[1])) for line in data[len(dots) + 1:]]
 
 def Part1():
-    dots1 = dots.copy()
     fold = intstructions[0]
-    removeDots = []
-    for index in range(len(dots1)):
-        dot = dots1[index]
+    dots1 = []
 
-        if fold[0] == "x":
-            diff = dot[0] - fold[1]
-
-            if diff > 0:
-                if (dot[0] - 2 * diff, dot[1]) in dots1:
-                    removeDots.append(dot)
-                else:
-                    dots1[index] = (dot[0] - 2 * diff, dot[1])
-        else:
-            diff = dot[1] - fold[1]
-
-            if diff > 0:
-                if (dot[0], dot[1] - 2 * diff) in dots1:
-                    removeDots.append(dot)
-                else:
-                    dots1[index] = (dot[0], dot[1] - 2 * diff)
-
-    for dot in removeDots:
-        dots1.remove(dot)
+    if fold[0] == "x":
+        dots1 = [dot if diff < 0 else
+                 (fx, dot[1]) for dot in dots if (diff := dot[0] - fold[1]) < 0 or
+                                                 ((fx := dot[0] - 2 * diff), dot[1]) not in dots]
+    else:
+        dots1 = [dot if diff < 0 else
+                 (dot[0], fy) for dot in dots if (diff := dot[1] - fold[1]) < 0 or
+                                                 (dot[0], (fy := dot[1] - 2 * diff)) not in dots]
 
     return len(dots1)
 
 def Part2():
+    global dots
+
     for fold in intstructions:
-        removeDots = []
-
-        for index in range(len(dots)):
-            dot = dots[index]
-
-            if fold[0] == "x":
-                diff = dot[0] - fold[1]
-                if diff > 0:
-                    if (dot[0] - 2 * diff, dot[1]) in dots:
-                        removeDots.append(dot)
-                    else:
-                        dots[index] = (dot[0] - 2 * diff, dot[1])
-            else:
-                diff = dot[1] - fold[1]
-                if diff > 0:
-                    if (dot[0], dot[1] - 2 * diff) in dots:
-                        removeDots.append(dot)
-                    else:
-                        dots[index] = (dot[0], dot[1] - 2 * diff)
-
-        for dot in removeDots:
-            dots.remove(dot)
+        if fold[0] == "x":
+            dots = [dot if diff < 0 else
+                    (fx, dot[1]) for dot in dots if (diff := dot[0] - fold[1]) < 0 or
+                                                    ((fx := dot[0] - 2 * diff), dot[1]) not in dots]
+        else:
+            dots = [dot if diff < 0 else
+                    (dot[0], fy) for dot in dots if (diff := dot[1] - fold[1]) < 0 or
+                                                    (dot[0], (fy := dot[1] - 2 * diff)) not in dots]
 
     maxX = max([dot[0] for dot in dots])
     maxY = max([dot[1] for dot in dots])
 
-    printDots = [["#" if (x, y) in dots else "." for x in range(maxX + 1)] for y in range(maxY + 1)]
+    printDots = [["█" if (x, y) in dots else
+                  " " for x in range(maxX + 1)] for y in range(maxY + 1)]
+
     for line in printDots:
         print("".join(line))
 
-    return (maxX, maxY)
-
 print(Part1())
-print(Part2())
+Part2()
